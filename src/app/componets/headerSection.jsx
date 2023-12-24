b4b2cae8c18126bf8ae11af531b5ac2e87be539b
Farouk Adeleke <110058327+Fadeleke57@users.noreply.github.com>
2023-12-24 02:35:03 -0500

    Updated header-section with parallax effect --- Using GSAP

--- src/app/componets/headerSection.jsx
+++ src/app/componets/headerSection.jsx
@@ -6,11 +6,35 @@ import Button from './buttons'
 import { Fade } from "react-awesome-reveal";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import BackgroundImage from './images/pawprint2edited.jpg'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from 'react';
 
 
 function Header() {
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    useEffect(() => {
+      // Banner parallax animation
+      gsap.to(".bg-image", {
+        y: 300, // Move on the y axis
+        scrollTrigger: {
+          trigger: ".header-gsap-animation", // Use header class as animation trigger
+          start: "top top", // Set trigger start position
+          scrub: true, // Have the animation follow the scroll position
+        }
+      });
+    }, []); // Empty dependency array ensures this runs once after initial render
+
     return (
-      <header>
+      <header className='header-gsap-animation'>
+
+      <div className='bg-image'>
+    
+      </div>
+
         <div className='header-content-div'>
             <Fade cascade direction='up' triggerOnce>
               <div className='header-logo-div'>
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 import styles from './page.module.css'
 import catLogo from './componets/images/cat_logo.svg'
